9805a7cbe57caf87a38a3400b683ee794eaba3cf
Lukáš Urban <lukas.urbann@seznam.cz>
2026-02-20 19:49:50 +0100

    oopsie

---
+++ Assets/_Code/Dialogues/Dialogue.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Dialogues
+{
+    [CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue", order = 1)]
+    public class Dialogue : ScriptableObject
+    {
+        public string characterName;
+        public string mainDialogue;
+
+        public void EvaluateResponseType(ResponseType type)
+        {
+            
+        }
+    }
+}
---
+++ Assets/_Code/Dialogues/DialogueManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Dialogues
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        public static DialogueManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+    
+    
+    }
+}
