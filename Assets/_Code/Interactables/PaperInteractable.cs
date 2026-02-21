4fa2b4b823985b4893b0a8830121a5cf4377d49d
Sam <162630226+ssl0v@users.noreply.github.com>
2026-02-21 23:07:35 +0100

    change quill and paper interact

--- Assets/_Code/Interactables/PaperInteractable.cs
+++ Assets/_Code/Interactables/PaperInteractable.cs
@@ -1,40 +1,57 @@
 using UnityEngine;
 using System.Collections;
 
-public class PaperInteractable : InteractableBase
+public class PaperInteractable : DraggableBase
 {
     [SerializeField] private VoteZone leftZone;
     [SerializeField] private VoteZone rightZone;
     [SerializeField] private float slideSpeed = 5f;
     [SerializeField] private float offScreenY = -20f;
     [SerializeField] private float returnDelay = 0.1f;
+    [SerializeField] private float swipeDownThreshold = 1f;
 
     private Vector3 _initialPosition;
+    private bool _isAnimating;
 
-    protected void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         _initialPosition = transform.position;
     }
 
-    protected override void OnClick()
+    protected override void Update()
     {
-        StartCoroutine(SlideOffAndBack());
+        
+    }
+
+    protected override void OnDragEnd()
+    {
+        base.OnDragEnd();
+        float draggedDown = _initialPosition.y - transform.position.y;
+        if (draggedDown > swipeDownThreshold && !_isAnimating)
+            StartCoroutine(SlideOffAndBack());
+        else if (!_isAnimating)
+            transform.position = _initialPosition;
     }
 
     public void OnDecisionFinalized()
     {
-        StartCoroutine(SlideOffAndBack());
+        if (!_isAnimating)
+            StartCoroutine(SlideOffAndBack());
     }
 
     private IEnumerator SlideOffAndBack()
     {
+        _isAnimating = true;
+        transform.position = _initialPosition;
+
         Vector3 offScreen = new Vector3(_initialPosition.x, offScreenY, _initialPosition.z);
-    
+
         while (Vector3.Distance(transform.position, offScreen) > 0.01f)
         {
             Vector3 prev = transform.position;
             transform.position = Vector3.Lerp(transform.position, offScreen, Time.deltaTime * slideSpeed);
-            Vector3 delta = transform.position - prev;
+            Vector3 delta = transform.position - prev;  
             leftZone.ShiftInk(delta);
             rightZone.ShiftInk(delta);
             yield return null;
@@ -52,5 +69,11 @@ public class PaperInteractable : InteractableBase
         }
 
         transform.position = _initialPosition;
+        _isAnimating = false;
+    }
+    
+    protected override bool CanDrag()
+    {
+        return !QuillInteractable.IsPickedUp && !_isAnimating;
     }
 }
