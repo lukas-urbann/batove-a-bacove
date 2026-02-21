8cbd8479e6fcec5d5ccfdf0218f707715b75e410
Lukáš Urban <lukas.urbann@seznam.cz>
2026-02-21 23:15:23 +0100

    Revert "Merge branch 'testing' into lukas"
    This reverts commit 8831921cbc84bf4abbb17da3dcdb7bacfb53b479, reversing
    changes made to 86f850758a0700a26c626559b2830dc28bbc84db.

--- Assets/_Code/Interactables/PaperInteractable.cs
+++ Assets/_Code/Interactables/PaperInteractable.cs
@@ -1,57 +1,40 @@
 using UnityEngine;
 using System.Collections;
 
-public class PaperInteractable : DraggableBase
+public class PaperInteractable : InteractableBase
 {
     [SerializeField] private VoteZone leftZone;
     [SerializeField] private VoteZone rightZone;
     [SerializeField] private float slideSpeed = 5f;
     [SerializeField] private float offScreenY = -20f;
     [SerializeField] private float returnDelay = 0.1f;
-    [SerializeField] private float swipeDownThreshold = 1f;
 
     private Vector3 _initialPosition;
-    private bool _isAnimating;
 
-    protected override void Awake()
+    protected void Awake()
     {
-        base.Awake();
         _initialPosition = transform.position;
     }
 
-    protected override void Update()
+    protected override void OnClick()
     {
-        
-    }
-
-    protected override void OnDragEnd()
-    {
-        base.OnDragEnd();
-        float draggedDown = _initialPosition.y - transform.position.y;
-        if (draggedDown > swipeDownThreshold && !_isAnimating)
-            StartCoroutine(SlideOffAndBack());
-        else if (!_isAnimating)
-            transform.position = _initialPosition;
+        StartCoroutine(SlideOffAndBack());
     }
 
     public void OnDecisionFinalized()
     {
-        if (!_isAnimating)
-            StartCoroutine(SlideOffAndBack());
+        StartCoroutine(SlideOffAndBack());
     }
 
     private IEnumerator SlideOffAndBack()
     {
-        _isAnimating = true;
-        transform.position = _initialPosition;
-
         Vector3 offScreen = new Vector3(_initialPosition.x, offScreenY, _initialPosition.z);
-
+    
         while (Vector3.Distance(transform.position, offScreen) > 0.01f)
         {
             Vector3 prev = transform.position;
             transform.position = Vector3.Lerp(transform.position, offScreen, Time.deltaTime * slideSpeed);
-            Vector3 delta = transform.position - prev;  
+            Vector3 delta = transform.position - prev;
             leftZone.ShiftInk(delta);
             rightZone.ShiftInk(delta);
             yield return null;
@@ -69,11 +52,5 @@ public class PaperInteractable : DraggableBase
         }
 
         transform.position = _initialPosition;
-        _isAnimating = false;
-    }
-    
-    protected override bool CanDrag()
-    {
-        return !QuillInteractable.IsPickedUp && !_isAnimating;
     }
 }
