0de4a389abf3fea00150a2f448bd020add13ba55
Sam <162630226+ssl0v@users.noreply.github.com>
2026-02-22 08:12:29 +0100

    add char sprites

--- Assets/_Code/UI/RandomizeJudgedImage.cs
+++ Assets/_Code/UI/RandomizeJudgedImage.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Controllers;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +12,9 @@ namespace UI
         [SerializeField] private Image image;
         [SerializeField] private Sprite[] poorSprites;
         [SerializeField] private Sprite[] richSprites;
+        
+        private Queue<Sprite> _poorQueue = new Queue<Sprite>();
+        private Queue<Sprite> _richQueue = new Queue<Sprite>();
 
         private void Start()
         {
@@ -21,15 +26,22 @@ namespace UI
             GameManager.Instance.newJudgedTypeReady?.RemoveListener(Randomize);
         }
         
+        private Queue<Sprite> BuildQueue(Sprite[] sprites)
+        {
+            return new Queue<Sprite>(sprites.OrderBy(_ => Random.value));
+        }
+
         private void Randomize(JudgedType judgedType)
         {
             switch (judgedType)
             {
                 case JudgedType.Poor:
-                    image.sprite = poorSprites[Random.Range(0, poorSprites.Length)];
+                    if (_poorQueue.Count == 0) _poorQueue = BuildQueue(poorSprites);
+                    image.sprite = _poorQueue.Dequeue();
                     break;
                 case JudgedType.Rich:
-                    image.sprite = richSprites[Random.Range(0, richSprites.Length)];
+                    if (_richQueue.Count == 0) _richQueue = BuildQueue(richSprites);
+                    image.sprite = _richQueue.Dequeue();
                     break;
             }
         }
